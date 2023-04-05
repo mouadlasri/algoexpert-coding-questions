f82e712ed8773f0f4d2940b866371beee6ef2c0b
Mouad Lasri <mouadkor20@gmail.com>
2023-04-05 23:22:10 +0900

    Added Sorted Squarred Array folder and problem description

---
+++ SortedSquaredArray/sortedsquaredarray.py
@@ -0,0 +1,12 @@
+
+#   Write a function that takes in a non-empty array of integers that are sorted
+#   in ascending order and returns a new array of the same length with the squares
+#   of the original integers also sorted in ascending order.
+
+# sample input = [1, 2, 3, 5, 6, 8, 9]
+# sample output = [1, 4, 9, 25, 36, 64, 81]
+
+def sortedSquarredArray(array):
+    # Write your code here
+    
+    return []
