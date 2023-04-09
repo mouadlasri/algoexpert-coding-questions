6c09a54d97472cc995ee3894b4d16582de0c2e76
Mouad Lasri <mouadkor20@gmail.com>
2023-04-09 23:27:40 +0900

    Added linear time complexity solution with two pointers

--- SortedSquaredArray/sortedsquaredarray.py
+++ SortedSquaredArray/sortedsquaredarray.py
@@ -8,7 +8,6 @@
 
 def sortedSquaredArrayBruteforce(array):
 
-    
     # Write your code here
     if(not array):
         return
@@ -20,10 +19,47 @@ def sortedSquaredArrayBruteforce(array):
     sortedArray = sorted(res)
     return sortedArray
 
-input = [1, -62, 3, 5, 6, 8, 9]
-sorted = sortedSquaredArrayBruteforce(input)
-print(sorted)
 
-# Brute force method
+def sortedSquaredArrayLinearTime(array):
+    # start = array[0]
+    # end = array[-1]
+    smallerValueIdx = 0
+    largerValueIdx = len(array) - 1
+    sortedArray = [0 for _ in array]
+
+
+
+    for index in reversed(range(len(array))):
+
+        smallerValue = array[smallerValueIdx]
+        largerValue = array[largerValueIdx]
+
+        if (abs(smallerValue) > abs(largerValue)):
+            smallerValueIdx = smallerValueIdx + 1
+            sortedArray[index] = smallerValue * smallerValue
+
+        else:
+            largerValueIdx = largerValueIdx - 1
+            sortedArray[index] = largerValue * largerValue
+
+    return sortedArray
+
+input = [-7, -5, -4, 3, 6, 8, 9]
+sortedBruteForce = sortedSquaredArrayBruteforce(input)
+print(sortedBruteForce)
+
+sortedLinear = sortedSquaredArrayLinearTime(input)
+print(sortedLinear)
+
+
+## Brute force method
 # Generate all squares of the input array, then sort the array
 # Time complexity O(nlogn), space ccomplexity O(n)
+
+
+## Linear time complexity method - Hint is when we get an input that is already sorted by default
+#  Use two pointers to keep track of the smallest and largest values in the input
+#  array. Compare the absolute values of these smallest and largest values,
+#  square the larger absolute value, and place the square at the end of the
+#  output array, filling it up from right to left. Move the pointers accordingly,
+#  and repeat this process until the output array is filled.
